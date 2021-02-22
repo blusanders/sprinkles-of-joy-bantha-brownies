b009144501e40067a1f458ba95d96277c9ee17a4
samj0516 <samj0516@gmail.com>
2021-02-22 12:21:13 -0600

    contact form appears when clicked in nav, send button posts the form info to db, and form clears when submitted

--- scripts/customers/ContactForm.js
+++ scripts/customers/ContactForm.js
@@ -18,6 +18,8 @@ const renderContactForm = () => {
     <div>
         <fieldset>
             <legend>Contact Us</legend>
+            <label for="name">Name</label><br />
+            <input type="text" name="name" id="name"><br />
             <label for="phone">Phone Number</label><br />
             <input type="tel" name="phone" id="phone"><br />
             <label for="email">E-mail</label><br />
@@ -27,4 +29,60 @@ const renderContactForm = () => {
             <button id="saveContactForm">Send</button>
         </fieldset>
     </div>`
+}
+
+eventHub.addEventListener("click", event => {
+    if(event.target.id==='saveContactForm'){
+        let nameId = document.querySelector('#name').value
+        let phoneId = document.querySelector('#phone').value
+        let emailId = document.querySelector('#email').value
+        let messageId = document.querySelector('#message').value
+
+        const newContact = {
+            name: nameId,
+            phone: phoneId,
+            email: emailId,
+            message: messageId,
+        }
+        saveContactForm(newContact)
+        .then(()=>{
+            nameId= ""
+            phoneId = ""
+            emailId = ""
+            messageId = ""
+            ContactForm()
+        })
+    }
+})
+
+// Handle browser-generated click event in component
+// eventHub.addEventListener("click", clickEvent => {
+//     if (clickEvent.target.id === "saveNote") {
+//         const dateId = document.querySelector("#date").value
+//         // const suspectId = document.querySelector("#suspect").value
+//         const noteId = document.querySelector("#note").value
+//         const criminalId = document.querySelector("#noteForm--criminal").value
+//         // Make a new object representation of a note
+//         const newNote = {
+//             date: dateId,
+//             criminalId: parseInt(criminalId),
+//             note: noteId,
+//         }
+
+//         // Change API state and application state
+//         saveNote(newNote)
+//     }
+// })
+
+
+
+export const saveContactForm = contact => {
+    return fetch('http://localhost:8088/contacts', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(contact)
+    })
+   
 }
